1bb92cda4935eb88f5e29668008e1c627c230377
Flamie4k <107715815+flamie4k@users.noreply.github.com>
2023-11-01 17:46:02 +0545

    Create search.cpp

---
+++ HagguViewer/x64/search.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class data{
+    protected:
+    string name [6] = {"Kidnapped","The Scarlet Pimpernel","Great Expectations","The Adventures of Sherlock Holmes","Robin Hood","Robinson Crusoe"};
+    string author [6] = {"Robert Louis Stevenson","Baroness Orczy","Charles Dickens","Arthur Conan Doyle","British Folklore","Daniel Defoe"};
+    string ID[6] = {"GESN-0001","GESN-0002","GESN-0003","GESN-0004","GESN-0005","GESN-0006",};
+    string publication = "Ekta Publications";
+    int no_of_books;
+    int level;
+    public:     
+};

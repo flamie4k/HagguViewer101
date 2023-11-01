d32759ec6ad5e4b82737c5067931e4673757a5d8
Flamie4k <107715815+flamie4k@users.noreply.github.com>
2023-11-01 17:48:45 +0545

    Update search.cpp

--- HagguViewer/x64/search.cpp
+++ HagguViewer/x64/search.cpp
@@ -8,7 +8,7 @@ class data{
     string author [6] = {"Robert Louis Stevenson","Baroness Orczy","Charles Dickens","Arthur Conan Doyle","British Folklore","Daniel Defoe"};
     string ID[6] = {"GESN-0001","GESN-0002","GESN-0003","GESN-0004","GESN-0005","GESN-0006",};
     string publication = "Ekta Publications";
-    int no_of_books;
-    int level;
+    int no_of_books[6] = {5,5,5,5,5,5};
+    int level[6] = {4,6,5,5,2,3};
     public:     
 };
